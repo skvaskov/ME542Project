71d6bfee8f6d416f0b6a012420354d0c73ea6046
Sean Vaskov <skvaskov@umich.edu>
2017-04-16 15:37:52 -0400

    updateforserver

--- gpoptimal_fullbike_phase3.m
+++ gpoptimal_fullbike_phase3.m
@@ -121,60 +121,6 @@ gpops_states=output.result.solution.phase.state;
 % u_ol = spline(gpops_t, u_ol');
 % u_ol = ppval(u_ol, t(:));
 
-figure
-subplot(3,1,1)
-plot(gpops_states(:,1),gpops_states(:,2))
-hold on
-plot(xc0(1),xc0(2),'k*')
-plot(xcF(1),xcF(2),'k*')
-plot(Track.bl(1,start:fin),Track.bl(2,start:fin),'r')
-plot(Track.br(1,start:fin),Track.br(2,start:fin),'r')
-xlabel('X')
-ylabel('Y')
-subplot(3,1,2)
-plot(gpops_t,u_ol(:,1))
-xlabel('time')
-ylabel('R')
-subplot(3,1,3)
-plot(gpops_t,u_ol(:,2))
-xlabel('time')
-ylabel('\gamma`')
-%-------------------------------------------------------------------------%
-%                          Functions                                      %
-%-------------------------------------------------------------------------%
-function phaseout = dyncont(input)
-t=input.phase.time;
-% x=input.phase.state(:,1);
-% y=input.phase.state(:,2);
-% psi=input.phase.state(:,3);
-% sigma=input.phase.state(:,4);
-% gamma=input.phase.state(:,5);
-x1=input.phase.state;
-u=input.phase.control;
-xcF=input.auxdata.xcF;
-xbl=input.auxdata.xblF;
-xbr=input.auxdata.xbrF;
-% R=input.phase.control(:,1);
-% gdot=input.phase.control(:,2);
-dx=zeros(length(t),size(x1,2));
-Ftire=zeros(length(t),2);
-   P = input.auxdata.cardata;
-    P = num2cell(P);
-for i=1:length(t)
-    X=num2cell(x1(i,:));
-    U = num2cell(u(i,:));
-    dx(i,:) = gpopsdyn(P{:},X{:},U{:});
-    Ftire(i,:)=tireforce(P{:},X{:},U{:});
-end
-phaseout.dynamics=dx;
-left=0.00584113818737157*x1(:,1).^2+0.000715776610793797*x1(:,2).^2+0.00410085041108458*x1(:,1).*x1(:,2)-2.0486203451185*x1(:,1)-0.733114404223798*x1(:,2)+167.248959747038;
-right=-0.00387663805047578*x1(:,1).^2-0.00150289791070212*x1(:,2).^2-0.00547027011226241*x1(:,1).*x1(:,2)+1.06576225836893*x1(:,1)+1.00011236472944*x1(:,2)-39.2651317560038;
-
-%phaseout.integrand=sqrt((x1(:,1)-ones(size(x1(:,1)))*xcF(1)).^2+(x1(:,2)-ones(size(x1(:,2)))*xcF(2)).^2);
-phaseout.integrand=x1(:,2)-xbr(2)-(xbl(2)-xbr(2))/(xbl(1)-xbr(1))*(x1(:,1)-xbr(1));
-phaseout.path=[left right Ftire];
-end
-
 function output=endpoint(input)
 Xf=input.phase(1).finalstate;
 T=input.phase(1).finaltime;
