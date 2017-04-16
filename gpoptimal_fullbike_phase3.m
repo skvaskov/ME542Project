ac03a325dcee923e3aeee09835ecf24938932c36
Sean Vaskov <skvaskov@umich.edu>
2017-04-16 15:38:54 -0400

    Update gpoptimal_fullbike_phase3.m

--- gpoptimal_fullbike_phase3.m
+++ gpoptimal_fullbike_phase3.m
@@ -121,17 +121,3 @@ gpops_states=output.result.solution.phase.state;
 % u_ol = spline(gpops_t, u_ol');
 % u_ol = ppval(u_ol, t(:));
 
-function output=endpoint(input)
-Xf=input.phase(1).finalstate;
-T=input.phase(1).finaltime;
-xbl=input.auxdata.xblF;
-xbr=input.auxdata.xbrF;
-xcF=input.auxdata.xcF;
-%end of phase 1 is determined by the crossing the line between the two
-%boundary points
-output.eventgroup(1).event=Xf(2)-xbr(2)-(xbl(2)-xbr(2))/(xbl(1)-xbr(1))*(Xf(1)-xbr(1));
-% objective is to minimize final time
-%compute total distance away from endpoint
-
-output.objective=input.phase(1).integral+1000*T+100*(Xf(3)-xcF(3))^2;
-end
